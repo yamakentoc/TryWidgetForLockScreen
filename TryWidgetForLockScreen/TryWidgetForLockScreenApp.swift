01c3737b5c0cc96bb9540f564940f61f67ed09de
yamakentoc <yamakentoc@gmail.com>
2022-08-01 22:54:51 +0900

    Initial Commit

---
+++ TryWidgetForLockScreen/TryWidgetForLockScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryWidgetForLockScreenApp.swift
+//  TryWidgetForLockScreen
+//
+//  Created by 山口賢登 on 2022/08/01.
+//
+
+import SwiftUI
+
+@main
+struct TryWidgetForLockScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
